5f40801990f6831e521d4646319fdc4e6f03013e
Elijah Gachuhi <elgachuhi@gmail.com>
2015-05-19 14:37:26 -0400

    routes and client-side dependencies

--- app.rb
+++ app.rb
@@ -4,3 +4,28 @@ require 'sinatra/base'
 
 
 set :database, {adapter: "sqlite3", database: "todo.sqlite3"}
+
+class Item < ActiveRecord::Base
+  # Item model
+end 
+
+get "/" do 
+  "ahoy"
+end 
+
+# Create
+
+post "/items" do 
+  puts params['items']
+end
+
+# Edit
+put "/item/:id" do 
+
+end 
+
+# Delete
+
+delete "/item/:id" do
+
+end 
